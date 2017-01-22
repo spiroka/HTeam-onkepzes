9173f07b5e22881cbe652b2a4ab8b3fb8e98a9b7
Gergő Takács <spiroka01@gmail.com>
2017-01-22 10:28:03 +0100

    Simple ListView with ArrayAdapter

--- app/src/main/java/hu/tgergo/hteam/MainActivity.java
+++ app/src/main/java/hu/tgergo/hteam/MainActivity.java
@@ -2,6 +2,8 @@ package hu.tgergo.hteam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] forecasts = new String[] {
+                "Hétfő - 20",
+                "Kedd - 22",
+                "Szerda - 21",
+                "Csütörtök - 24",
+                "Péntek - 18",
+                "Szombat - 16",
+                "Vasárnap - 20",
+        };
+
+        ArrayAdapter<String> forecastAdapter =
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_expandable_list_item_1,
+                        forecasts
+                );
+
+        ListView list = (ListView) findViewById(R.id.forecast_list);
+        list.setAdapter(forecastAdapter);
     }
 }
